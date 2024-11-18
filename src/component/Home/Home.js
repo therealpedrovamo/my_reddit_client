82ff51d4558624ec5a7b70b1a7536cdef8a5bbca
PedroV <pedrodjvm@hotmail.com>
2024-11-18 10:16:27 -0500

    Added a division on Home, for the posts

--- src/component/Home/Home.js
+++ src/component/Home/Home.js
@@ -2,8 +2,13 @@
 
 const Home= () =>{
     return (
-        <div>
-            <h1>My-Reddit-Client</h1>
+        <div className='HomePage'>
+            <div className='Title'>
+                <h1>My-Reddit-Client</h1>
+            </div>
+            <div className='Posts'>
+                <h2>Posts:</h2>
+            </div>
         </div>
     )
 }
